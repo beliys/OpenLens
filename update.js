247c7dabe11ce0c312ec8a0414498a4a9b58c443
Oleksander B <5033681+beliys@users.noreply.github.com>
2023-05-30 10:44:36 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,24 +1,13 @@
 const fs = require('fs');
 
-var packagejson = require('./lens/packages/open-lens/package.json');
+var packagejson = require('./lens/open-lens/package.json');
 
 packagejson.build.publish = [{
-    url: "https://github.com/beliys/OpenLens/releases/download/Latest",
+    url: "https://github.com/MuhammedKalkan/OpenLens/releases/download/Latest",
     provider: "generic"
 }];
 
 packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
+packagejson.build.npmRebuild = true;
 
-if (process.platform != "win32") {
-    // build both x86_64 and arm64 for Linux and Darwin
-    packagejson.scripts['build:app'] = "electron-builder --publish onTag --x64 --arm64";
-}
-
-fs.writeFileSync('./lens/packages/open-lens/package.json', JSON.stringify(packagejson));
-
-
-let npmrc = fs.readFileSync('./lens/.npmrc','utf-8');
-npmrc = npmrc.replace("disturl \"","disturl = \"")
-npmrc = npmrc.replace("target \"","target = \"")
-npmrc = npmrc.replace("runtime \"","runtime = \"")
-fs.writeFileSync('./lens/.npmrc',npmrc)
+fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packagejson));
